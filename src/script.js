a33ed986048ba778d3a86ab10c87e8d5ed97b04e
AryanNoSensie <offciearyan14@gail.com>
2025-02-14 09:40:45 +0530

    completed the hero section and now building company slider

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+const navDialog = document.getElementById('nav-dialog');
 function handleMenu(){
-    
+    navDialog.classList.toggle('hidden')
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors : {
+        "primary" : "#3238f2"
+      },
       fontFamily : {
         "display" : ['Poppins ', 'sans-serif'],
         "body" : ['Inter' ,'sans-serif']
